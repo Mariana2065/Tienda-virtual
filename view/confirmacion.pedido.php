6646c0baa2b5a2ba161970460615ca8e7c57a750
Mariana2065 <marmillan1@soy.sena.edu.co>
2025-06-25 18:54:56 -0500

    agrego la interfaz de confirmación de pedido

---
+++ view/confirmacion.pedido.php
@@ -0,0 +1,33 @@
+<?php include '../includes/header.php'; ?>
+
+<div class="container">
+
+    <!-- Encabezado -->
+    <h1 class="encabezado-confirmar text-center mb-4">PEDIDO CONFIRMADO</h1>
+    <p><strong>¡Pedido confirmado con éxito!</strong><br>
+        Gracias por tu compra. Por favor, realiza la transferencia del total acordado a la cuenta [XXXX-XXXX-XXXX] y tu pedido será procesado y enviado a la brevedad.</p>
+
+    <!-- Datos del pedido -->
+    <div class="mt-4">
+        <h4>Datos del pedido</h4>
+        <p><strong>Referencia del pedido:</strong> 001</p>
+        <p><strong>Total a pagar:</strong> $00000</p>
+        <div class="linea"></div>
+    </div>
+
+   <div class="producto-confirmacion">
+  <div class="row g-0 align-items-center">
+    <div class="col-4 imagen-producto">
+      <img src="../assets/icon-powerly.png" alt="" class="img-producto">
+    </div>
+    <div class="col-8 p-3">
+      <p class="nombre">Nombre</p>
+      <p class="precio">$00000</p>
+      <p class="cantidad text-muted">Cantidad : X</p>
+    </div>
+  </div>
+</div>
+
+</div>
+
+    <?php include '../includes/footer.php'; ?>
