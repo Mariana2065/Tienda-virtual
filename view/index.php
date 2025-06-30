4cf7b290d7db61035ef11e22dcbcce89207a59df
Mariana2065 <marmillan1@soy.sena.edu.co>
2025-06-30 16:13:03 -0500

    intengro el index o inicio de la pag

---
+++ view/index.php
@@ -0,0 +1,65 @@
+<?php include '../includes/header.php'; ?>
+
+<!-- principal -->
+<section class="contenedor-imagen-index">
+  <div class="container contenedor-texto-hero">
+    <div class="caja-texto-izquierda">
+      <h1 class="titulo-index">Bienvenido a <span class="powerly">POWERLY</span></h1>
+      <h2 class="subtitulo-index">Activa tu poder. Viste con energía.</h2>
+      <p class="descripcion-index">Descubre ropa deportiva diseñada para tu mejor versión.</p>
+    </div>
+  </div>
+</section>
+
+
+<section class="frase-destacada">
+  <div class="container text-center">
+    <h2 class="titulo-frase">Más que ropa deportiva…</h2>
+    <p class="texto-frase">
+      En <span class="marca">POWERLY</span> te acompañamos a romper tus límites con estilo, comodidad y actitud.
+    </p>
+  </div>
+</section>
+
+
+<!-- Categorías Destacadas -->
+<section id="categorias" class="seccion-categorias">
+  <div class="container">
+    <h2 class="text-center mb-5">Categorías destacadas</h2>
+    <div class="row justify-content-center">
+      <!-- Ropa de hombre -->
+      <div class="col-md-4 mb-4">
+        <div class=" categoria-index-card">
+          <img src="../assets/icon-powerly.png" class="card-img-top" alt="Ropa de hombre">
+          <div class="card-body text-center">
+            <h5 class="card-title">Ropa de hombre</h5>
+          </div>
+        </div>
+      </div>
+      <!-- Ropa de mujer -->
+      <div class="col-md-4 mb-4">
+        <div class=" categoria-index-card">
+          <img src="../assets/icon-powerly.png" class="card-img-top" alt="Ropa de mujer">
+          <div class="card-body text-center">
+            <h5 class="card-title">Ropa de mujer</h5>
+          </div>
+        </div>
+      </div>
+      <!-- Calzado -->
+      <div class="col-md-4 mb-4">
+        <div class=" categoria-index-card">
+          <img src="../issets/icon-powerly.png" class="card-img-top" alt="Calzado">
+          <div class="card-body text-center">
+            <h5 class="card-title">Calzado</h5>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="text-center mt-4">
+      <a href="#" class="btn btn-explorar-productos">Explorar productos</a>
+    </div>
+  </div>
+</section>
+
+<!-- Footer -->
+<?php include '../includes/footer.php'; ?>
